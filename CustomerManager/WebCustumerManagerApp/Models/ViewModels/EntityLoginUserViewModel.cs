78bbfde329a4fa2c935fde54ce8cd5a6f063f186
Fabricio Araujo Sousa <fabricio.sousa@eldorado.org.br>
2017-09-06 08:29:59 -0300

    06_06 login autenthication and encode password feature

---
+++ CustomerManager/WebCustumerManagerApp/Models/ViewModels/EntityLoginUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebCustumerManagerApp.Models.ViewModels
+{
+    public class EntityLoginUserViewModel
+    {
+        [StringLength(50)]
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Inform your user name", AllowEmptyStrings = false)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Inform your password", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
